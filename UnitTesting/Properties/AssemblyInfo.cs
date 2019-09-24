04bb7db9a889d1cf6dc41080ef5ec035c1d6668d
Admin <Admin@DESKTOP-89NTJEN>
2019-09-24 15:38:39 +1200

    Code

---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b288277-926c-4b83-8d77-ea7e7a642965")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTesting/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VedioRental;
+
+namespace DbTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //defining an object with in the class Database from VedioRental
+        VedioRental.Database.Database DBTest = new VedioRental.Database.Database();
+        [TestMethod]
+        public void TestDBConection()
+        {
+            // Variable of actual and expected connection String 
+            var ActualDBCon = DBTest.constring;
+            var ExpexctedDBCon = @"Data Source=DESKTOP-89NTJEN\SQLEXPRESS;Initial Catalog=VideoRental;Integrated Security=True";
+            //Assert - checking the output is which expected
+            Assert.AreEqual(ExpexctedDBCon, ActualDBCon);
+        }
+    }
+}
